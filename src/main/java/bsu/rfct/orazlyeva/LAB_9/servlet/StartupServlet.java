d95edd4da29a887c3d3efd12f54723aead174b4c
GuljemalOrazlyeva <gul2000jemal@yandex.ru>
2021-05-31 22:50:11 +0300

    ADDED:servlet class

---
+++ src/main/java/bsu/rfct/orazlyeva/LAB_9/servlet/StartupServlet.java
@@ -0,0 +1,25 @@
+package bsu.rfct.orazlyeva.LAB_9.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class StartupServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        UserList userList = UserListHelper.readUserList(getServletContext());
+        getServletContext().setAttribute("users", userList);
+        AdList adList = AdListHelper.readAdList(getServletContext());
+        getServletContext().setAttribute("ads", adList);
+
+        for (Ad ad: adList.getAds()) {
+            ad.setAuthor(userList.findUser(ad.getAuthorId()));
+            ad.setLastModified(ad.getLastModified());
+        }
+    }
+
+}
